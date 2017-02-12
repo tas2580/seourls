tas2580/seourls

commit 375ac8d796f8083c74fba20bd4891f051b5826eb

fixed pagination link for forum

event/listener.php
@@ -253,13 +253,13 @@ class listener implements EventSubscriberInterface
 		}
 
 		$start = (($event['on_page'] - 1) * $event['per_page']);
-		if (!empty($this->topic_title))
+		if (!empty($this->topic_data) && isset($param['f']) && isset($param['t']))
 		{
-			$event['generate_page_link_override'] = append_sid($this->base->generate_topic_link($this->forum_id, $this->forum_title, $this->topic_id, $this->topic_title, $start));
+			$event['generate_page_link_override'] = append_sid($this->base->generate_topic_link($this->topic_data['forum_id'], $this->topic_data['forum_name'], $this->topic_data['topic_id'], $this->topic_data['topic_title'], $start));
 		}
-		else if (!empty($this->forum_title))
+		else if (!empty($this->forum_data) && isset($param['f']))
 		{
-			$event['generate_page_link_override'] = append_sid($this->base->generate_forum_link($this->forum_id, $this->forum_title, $start));
+			$event['generate_page_link_override'] = append_sid($this->base->generate_forum_link($this->forum_data['forum_id'], $this->forum_data['forum_name'], $start));
 		}
 	}
 
@@ -293,15 +293,19 @@ class listener implements EventSubscriberInterface
 	 */
 	public function viewforum_modify_topicrow($event)
 	{
-		$topic_row = $event['topic_row'];
-		$this->forum_title = $topic_row['FORUM_NAME'];
-		$this->forum_id = $topic_row['FORUM_ID'];
-		$this->topic_title = $topic_row['TOPIC_TITLE'];
-		$this->topic_id = $topic_row['TOPIC_ID'];
+		// assign to be used in pagination_generate_page_link
+		$this->topic_data = array(
+			'forum_id' => $event['topic_row']['FORUM_ID'],
+			'forum_name' => $event['topic_row']['FORUM_NAME'],
+			'topic_id' => $event['topic_row']['TOPIC_ID'],
+			'topic_title' => $event['topic_row']['TOPIC_TITLE']
+		);
 
-		$u_view_topic = $this->base->generate_topic_link($this->forum_id, $this->forum_title, $this->topic_id, $this->topic_title);
+		$topic_row = $event['topic_row'];
+
+		$u_view_topic = $this->base->generate_topic_link($topic_row['FORUM_ID'], $topic_row['FORUM_TITLE'], $topic_row['TOPIC_ID'], $topic_row['TOPIC_TITLE']);
 		$topic_row['U_VIEW_TOPIC'] = append_sid($u_view_topic);
-		$topic_row['U_VIEW_FORUM'] = append_sid($this->base->generate_forum_link($this->forum_id, $this->forum_title));
+		$topic_row['U_VIEW_FORUM'] = append_sid($this->base->generate_forum_link($topic_row['FORUM_ID'], $topic_row['FORUM_NAME']));
 		$topic_row['U_LAST_POST'] = append_sid($this->base->generate_lastpost_link($event['topic_row']['REPLIES'], $u_view_topic) . '#p' . $event['row']['topic_last_post_id']);
 		$topic_row['U_NEWEST_POST'] = append_sid($this->base->generate_lastpost_link($event['topic_row']['REPLIES'], $u_view_topic) . '#unread');
 
@@ -317,9 +321,15 @@ class listener implements EventSubscriberInterface
 	 */
 	public function viewforum_get_topic_data($event)
 	{
+		// assign to be used in pagination_generate_page_link
+		$this->forum_data = array(
+			'forum_id' => $event['forum_data']['forum_id'],
+			'forum_name' => $event['forum_data']['forum_name']
+		);
+
 		$start = $this->request->variable('start', 0);
 		$this->template->assign_vars(array(
-			'U_VIEW_FORUM'	=> append_sid($this->base->generate_forum_link($event['forum_data']['forum_id'], $event['forum_data']['forum_name'], $start)),
+			'U_VIEW_FORUM'		=> append_sid($this->base->generate_forum_link($event['forum_data']['forum_id'], $event['forum_data']['forum_name'], $start)),
 			'U_CANONICAL'		=> $this->base->generate_forum_link($event['forum_data']['forum_id'], $event['forum_data']['forum_name'], $start, true),
 		));
 	}
@@ -350,10 +360,13 @@ class listener implements EventSubscriberInterface
 	 */
 	public function viewtopic_assign_template_vars_before($event)
 	{
-		$this->forum_title = $event['topic_data']['forum_name'];
-		$this->forum_id = $event['topic_data']['forum_id'];
-		$this->topic_title = $event['topic_data']['topic_title'];
-		$this->topic_id = $event['topic_data']['topic_id'];
+		// assign to be used in pagination_generate_page_link
+		$this->topic_data = array(
+			'forum_id' => $event['topic_data']['forum_id'],
+			'forum_name' => $event['topic_data']['forum_name'],
+			'topic_id' => $event['topic_data']['topic_id'],
+			'topic_title' => $event['topic_data']['topic_title']
+		);
 	}
 
 	public function viewtopic_before_f_read_check()

commit 1cb98c73e1c3345733c79c5f5d6399b7b767ca22

Fix forum title not appearing in topic links
https://github.com/tas2580/seourls/commit/375ac8d796f8083c74fba20bd4891f051b5826eb Introduced a small bug due to using wrong array index name

event/listener.php
@@ -320,7 +320,7 @@ class listener implements EventSubscriberInterface
 
 		$topic_row = $event['topic_row'];
 
-		$u_view_topic = $this->base->generate_topic_link($topic_row['FORUM_ID'], $topic_row['FORUM_TITLE'], $topic_row['TOPIC_ID'], $topic_row['TOPIC_TITLE']);
+		$u_view_topic = $this->base->generate_topic_link($topic_row['FORUM_ID'], $topic_row['FORUM_NAME'], $topic_row['TOPIC_ID'], $topic_row['TOPIC_TITLE']);
 		$topic_row['U_VIEW_TOPIC'] = append_sid($u_view_topic);
 		$topic_row['U_VIEW_FORUM'] = append_sid($this->base->generate_forum_link($topic_row['FORUM_ID'], $topic_row['FORUM_NAME']));
 		$topic_row['U_LAST_POST'] = append_sid($this->base->generate_lastpost_link($event['topic_row']['REPLIES'], $u_view_topic) . '#p' . $event['row']['topic_last_post_id']);