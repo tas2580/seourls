af1cbeba98555ff29aa2df01384db893cd529c92
tas2580 <tobi.schaefer@gmail.com>
2017-12-28 06:00:01 +0100

    update travis

--- travis/prepare-epv.sh
+++ travis/prepare-epv.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 #
-# PayPal Donation extension for the phpBB Forum Software package.
+# This file is part of the phpBB Forum Software package.
 #
-# @copyright (c) 2016 Matt Friedman
+# @copyright (c) phpBB Limited <https://www.phpbb.com>
 # @license GNU General Public License, version 2 (GPL-2.0)
+#
+# For full copyright and license information, please see
+# the docs/CREDITS.txt file.
 #
 set -e
 set -x
 
-DB=$1
-TRAVIS_PHP_VERSION=$2
+EPV=$1
+NOTESTS=$2
 
-if [ "$DB" == "mysqli" -a "$TRAVIS_PHP_VERSION" == "5.5" ]
+if [ "$EPV" == "1" -a "$NOTESTS" == "1" ]
 then
 	cd phpBB
-	composer require phpbb/epv:dev-master --dev --no-interaction
+	composer remove sami/sami --update-with-dependencies --dev --no-interaction
+	composer require phpbb/epv:dev-master --dev --no-interaction --ignore-platform-reqs
 	cd ../
 fi
